007eabe7b081467273be31eb9b70067ea09b3614
aum1110 <aumsohoni1110@gmail.com>
2022-01-30 15:33:55 +0530

    Crouch&jump

--- Assets/Game Assets/Scripts/Movement.cs
+++ Assets/Game Assets/Scripts/Movement.cs
@@ -6,24 +6,54 @@ public class Movement : MonoBehaviour
 {
     public Animator anim;
     private SpriteRenderer sr;
+    public Sprite Standing;
+    public Sprite crouch;
+    public BoxCollider2D boxCollider2D;
+    public Vector2 StandingSize;
+    public Vector2 crouchingsize;
+    private BoxCollider2D coll;
+    [SerializeField] private LayerMask jumpableground;
     [SerializeField] private Rigidbody2D rb;
     public float speed = 5f;
+   
     public float jumpforce = 5f;
-    void Start()
+   
+
+
+void Start()
     {
+        boxCollider2D = GetComponent<BoxCollider2D>();
         sr = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
+        StandingSize = boxCollider2D.size;
         
-    }
+    
+}
 
     
     void Update()
     {
+
         float xDirection = Input.GetAxisRaw("Horizontal");
+        float yDirection = Input.GetAxisRaw("Vertical");
+
         anim.SetFloat("Speed",Mathf.Abs(xDirection));
+        
         rb.velocity = new Vector2(speed * xDirection, rb.velocity.y);
 
-        if (xDirection < 0)
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+
+            Debug.Log("Jump"); 
+            rb.velocity = Vector2.up * jumpforce;
+            anim.SetBool("jump", true);
+
+        }
+      
+
+       if (xDirection < 0)
         {
             sr.flipX = true;
         }
@@ -32,6 +62,37 @@ public class Movement : MonoBehaviour
             sr.flipX = false;
         
         }
+      
+
+        if (Input.GetKey(KeyCode.LeftControl))
+            {
+            anim.SetBool("Crouch",true);
+            sr.sprite = crouch;
+            boxCollider2D.size = crouchingsize;
+        
+        
+            }
+
+
+       
         
     }
+
+    private void Nojump()
+        {
+
+        anim.SetBool("jump", false);
+        }
+
+    private void Nocrouch()
+    {
+
+        anim.SetBool("Crouch", false);
+        sr.sprite = Standing;
+        boxCollider2D.size = StandingSize;
+
+    }
+
+
+
 }
